9ba4734c2a1c098884189f97148d574ca77987cd
Nicholas <nicxzmiller2@gmail.com>
2020-04-19 00:28:34 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+var map;
+      var markers = [];
+      var infoWindow;
+      var locationSelect;
+
+function initMap() {
+          var malta = {lat: 35.9375, lng: 14.3754};
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: malta,
+            zoom: 11,
+            mapTypeId: 'roadmap',
+          });
+        }
