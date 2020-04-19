5ca57ef01c6bd6da7a0dd00683a1e83f7f7ec2d9
Nicholas <nicxzmiller2@gmail.com>
2020-04-19 23:18:10 +0200

    added store markers and info window

--- js/index.js
+++ js/index.js
@@ -5,10 +5,43 @@ var infoWindow;
 var locationSelect;
 
 function initMap() {
-      var malta = {lat: 35.9375, lng: 14.3754};
+      var losAngeles = {lat: 34.063380, lng: -118.358080};
       map = new google.maps.Map(document.getElementById('map'), {
-        center: malta,
+        center: losAngeles,
         zoom: 11,
         mapTypeId: 'roadmap',
   });
+    infoWindow = new google.maps.InfoWindow();
+
+    showStoreMarkers();
+
+}
+
+function showStoreMarkers() {
+    var bounds = new google.maps.LatLngBounds();
+    stores.forEach(function (store, index) {
+        var latlng = new google.maps.LatLng(
+            store.coordinates.latitude,
+            store.coordinates.longitude);
+
+        var name = store.name;
+        var address = store.addressLines[0];
+        createMarker(latlng, name, address, index);
+        bounds.extend(latlng);
+    })
+    map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address, index) {
+    var html = "<b>" + name + "</b> <br/>" + address;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+        label: `${index+1}`
+    });
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(html);
+        infoWindow.open(map, marker);
+    });
+    markers.push(marker);
 }
