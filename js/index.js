b8d887e4604a5a3d9d64a56742d487e67ed1fb03
Nicholas <nicxzmiller2@gmail.com>
2020-04-19 05:06:36 +0200

    adding google-map API key

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 
 var map;
-      var markers = [];
-      var infoWindow;
-      var locationSelect;
+var markers = [];
+var infoWindow;
+var locationSelect;
 
 function initMap() {
-          var malta = {lat: 35.9375, lng: 14.3754};
-          map = new google.maps.Map(document.getElementById('map'), {
-            center: malta,
-            zoom: 11,
-            mapTypeId: 'roadmap',
-          });
-        }
+      var sydney = {lat: -33.863276, lng: 151.107977};
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: sydney,
+        zoom: 11,
+        mapTypeId: 'roadmap',
+  });
+}
