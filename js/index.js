890aa28b8a109a370d65458363396a4b34f743c4
Nicholas <nicxzmiller2@gmail.com>
2020-04-20 04:10:53 +0200

    redesigning map

--- js/index.js
+++ js/index.js
@@ -9,6 +9,86 @@ function initMap() {
       map = new google.maps.Map(document.getElementById('map'), {
         center: losAngeles,
         zoom: 11,
+          styles: [
+              {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+              {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+              {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+              {
+                  featureType: 'administrative.locality',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+              },
+              {
+                  featureType: 'poi',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+              },
+              {
+                  featureType: 'poi.park',
+                  elementType: 'geometry',
+                  stylers: [{color: '#263c3f'}]
+              },
+              {
+                  featureType: 'poi.park',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#6b9a76'}]
+              },
+              {
+                  featureType: 'road',
+                  elementType: 'geometry',
+                  stylers: [{color: '#38414e'}]
+              },
+              {
+                  featureType: 'road',
+                  elementType: 'geometry.stroke',
+                  stylers: [{color: '#212a37'}]
+              },
+              {
+                  featureType: 'road',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#9ca5b3'}]
+              },
+              {
+                  featureType: 'road.highway',
+                  elementType: 'geometry',
+                  stylers: [{color: '#746855'}]
+              },
+              {
+                  featureType: 'road.highway',
+                  elementType: 'geometry.stroke',
+                  stylers: [{color: '#1f2835'}]
+              },
+              {
+                  featureType: 'road.highway',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#f3d19c'}]
+              },
+              {
+                  featureType: 'transit',
+                  elementType: 'geometry',
+                  stylers: [{color: '#2f3948'}]
+              },
+              {
+                  featureType: 'transit.station',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#d59563'}]
+              },
+              {
+                  featureType: 'water',
+                  elementType: 'geometry',
+                  stylers: [{color: '#17263c'}]
+              },
+              {
+                  featureType: 'water',
+                  elementType: 'labels.text.fill',
+                  stylers: [{color: '#515c6d'}]
+              },
+              {
+                  featureType: 'water',
+                  elementType: 'labels.text.stroke',
+                  stylers: [{color: '#17263c'}]
+              }
+          ],
         mapTypeId: 'roadmap',
   });
     infoWindow = new google.maps.InfoWindow();
@@ -90,20 +170,26 @@ function showStoreMarkers(stores) {
             store.coordinates.latitude,
             store.coordinates.longitude);
 
+        var number = store.phoneNumber;
+        var open = store.openStatusText;
         var name = store.name;
         var address = store.addressLines[0];
-        createMarker(latlng, name, address, index);
+        createMarker(latlng, name, address, number, open, index);
         bounds.extend(latlng);
     });
     map.fitBounds(bounds);
 }
 
-function createMarker(latlng, name, address, index) {
-    var html = "<b>" + name + "</b> <br/>" + address;
+function createMarker(latlng, name, address, number, open, index) {
+    var image = 'images/moneyheist.png';
+    var html = "<b>" + name + "</b> <br><br/>" + address + "<br> <br/> <hr>" + "<i class='fas fa-phone-alt'></i> " + number + "<br> <br/> <hr>" + open;
     var marker = new google.maps.Marker({
         map: map,
         position: latlng,
-        label: `${index+1}`
+        number: number,
+        open:open,
+        icon:image
+        // label: `${index+1}`
     });
     google.maps.event.addListener(marker, 'click', function() {
         infoWindow.setContent(html);
